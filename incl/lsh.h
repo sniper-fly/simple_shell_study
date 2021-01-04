1d16a6566a126751f2995c3bb6b96fa297fb2308
rnakai <rnakai@student.42tokyo.jp>
2021-01-04 22:40:42 +0900

    create: lsh_read_line

--- incl/lsh.h
+++ incl/lsh.h
@@ -1,7 +1,6 @@
 #ifndef LSH_H
 # define LSH_H
 
-# define EXIT_SUCCESS 0
 # define LSH_RL_BUFSIZE 1024
 
 void	lsh_loop(void);
--- src/lsh_read_line.c
+++ src/lsh_read_line.c
@@ -1,7 +1,59 @@
 #include "lsh.h"
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 char	*lsh_read_line(void)
 {
-	static char ch = 'c';
-	return (&ch);
+	int		bufsize = LSH_RL_BUFSIZE;
+	int		position = 0;
+	char	*buffer = (char*)malloc(sizeof(char) * bufsize);
+	int		c;
+
+	if (!buffer)//allocation error
+		exit(EXIT_FAILURE);
+	
+	while (1)
+	{
+		c = getchar();
+
+		if (c == EOF || c == '\n')
+		{
+			buffer[position] = '\0';
+			return (buffer);
+		}
+		else
+		{
+			buffer[position] = c;
+		}
+		position++;
+
+		if (position >= bufsize)
+		{
+			bufsize += LSH_RL_BUFSIZE;
+			buffer = (char*)realloc(buffer, bufsize);
+			if (!buffer)//allocation error
+				exit(EXIT_FAILURE);
+		}
+		
+	}
 }
+
+// char	*lsh_read_line(void)
+// {
+// 	char	*line = NULL;
+// 	ssize_t	bufsize = 0;
+
+// 	if (getline(&line, &bufsize, stdin) == -1)
+// 	{
+// 		if (feof(stdin))
+// 		{
+// 			exit(EXIT_SUCCESS);
+// 		}
+// 		else
+// 		{
+// 			perror("readline");
+// 			exit(EXIT_FAILURE);
+// 		}
+// 	}
+// }
