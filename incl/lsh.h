e6830779f96b50f679d48d7585535bbac5edccdd
rnakai <rnakai@student.42tokyo.jp>
2021-01-04 22:14:37 +0900

    move:lsh.h

---
+++ incl/lsh.h
@@ -0,0 +1,10 @@
+#ifndef LSH_H
+# define LSH_H
+
+# define EXIT_SUCCESS 0
+# define LSH_RL_BUFSIZE 1024
+
+void	lsh_loop(void);
+char	*lsh_read_line(void);
+
+#endif
--- lsh.h
+++
@@ -1,8 +0,0 @@
-#ifndef LSH_H
-#define LSH_H
-
-#define EXIT_SUCCESS 0
-
-void	lsh_loop(void);
-
-#endif
