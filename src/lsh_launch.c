043b7d28bb1e5b0ad6d2bd4034c3b8fc021fedba
rnakai <rnakai@student.42tokyo.jp>
2021-01-06 11:00:16 +0900

    create: lsh_launch

---
+++ src/lsh_launch.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int		lsh_launch(char **args)
+{
+	pid_t	pid, wpid;
+	int		status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+		{
+			perror("lsh");
+		}
+		exit(EXIT_FAILURE);
+	} else if (pid < 0)
+		perror("lsh");
+	else
+	{
+		do {
+			wpid = waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return (1);
+}
+
+//WUNTRACED: 子プロセスが停止した場合にも復帰する (子プロセスが ptrace(2) でトレースされている場合は除く)。 このオプションが指定されていない場合でも、停止したプロセスが 「トレース (traced)」されていれば、子プロセスの状態が報告される。
